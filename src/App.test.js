c2132a370b52cabb4516c168ca59f6ee140a5c18
derekmcauley <derekmcauley7@gmail.com>
2022-12-14 13:02:41 +0000

    Changing main App component and updating tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { render, queryByAttribute} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders an h1", () => {
+  const { getByText } = render(<App />);
+
+  const h1 = getByText(/My Awesome Blog/);
+  
+  expect(h1).toHaveTextContent("My Awesome Blog");
+});
+
+test("renders an body div", () => {
+  const dom = render(<App />);
+  const getById = queryByAttribute.bind(null, 'id');
+
+  const bodyDiv = getById(dom.container, 'page-body');
+
+  expect(bodyDiv).toHaveTextContent("Welcome to my blog.");
 });
