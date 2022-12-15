0ec1ae4b107216a91f2137e71a200d43aa45012b
derekmcauley <derekmcauley7@gmail.com>
2022-12-15 16:48:32 +0000

    Moving the ArticlesList to a component so that it can be reused later

---
+++ src/components/ArticlesList.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+const ArticlesList = ( {articles} ) => {
+    return (
+        <>
+        {articles.map(
+            article => (
+            <div key={article.name}>
+                <Link  className="article-list-item" to={`/articles/${article.name}`}>
+                <h2>{article.title}</h2>
+                <p>{article.content[0].substring(0, 150)} ...</p>
+                </Link>
+            </div>
+        ))}
+        </>
+    )
+}
+
+export default ArticlesList
--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,18 +1,13 @@
 import articles from "./article-content";
-import { Link } from "react-router-dom";
+import ArticlesList from  "../components/ArticlesList"
 
 const ArticlesListPage = () => {
     return (
-        articles.map(
-            article => (
-            <div key={article.name}>
-                <h1>{article.title}</h1>
-                <p>{article.content.slice(0,1)}</p>
-                <Link to={`/articles/${article.name}`}>View </Link>
-            </div>
-            ))
+        <>
+            <h1>Articles</h1>
+            <ArticlesList articles={articles}/>
+        </>
     );
-    
 }
 
 export default ArticlesListPage;
