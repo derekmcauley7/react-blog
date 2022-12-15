4ea0fb375a159f0c249d57f0dbc583fb691270ba
derekmcauley <derekmcauley7@gmail.com>
2022-12-15 01:21:16 +0000

    Fixing test by adding BrowserRouter to render

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -12,6 +12,7 @@ const ArticlesListPage = () => {
             </div>
             ))
     );
+    
 }
 
 export default ArticlesListPage;
--- src/pages/AtriclesListPage.test.js
+++ src/pages/AtriclesListPage.test.js
@@ -1,8 +1,9 @@
 import { render, queryByAttribute} from '@testing-library/react';
 import ArticlesListPage from './ArticlesListPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-test("renders an h1", () => {
-  const { getByText } = render(<ArticlesListPage />);
+test("renders an h1 from mock data", () => {
+  const { getByText } = render(<BrowserRouter> <ArticlesListPage /> </BrowserRouter>);
 
   const h1 = getByText(/The Fastest Way to Learn React/);
   
